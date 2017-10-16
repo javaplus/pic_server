802bca6ce7cd23d8bf208f2e19a525685e1cb1fe
Barry Tarlton <TARLTOB1@nationwide.com>
2017-10-16 10:29:16 -0400

    can successfully upload multipart message with pic

--- file_uploader.py
+++ file_uploader.py
@@ -1,16 +1,18 @@
-#import requests
+import requests
 import io
 
 
 def uploadFile(fileData):
     # create temp file:
     #print("FileDate size=" + fileData.size())
+
+    #myfile = file(None, 'rb', fileData)
+    files = {'file': ('mypic.jpg',io.BytesIO(fileData), 'image/jpeg', {'Expires': '0'})}
+    res = requests.post(url='http://localhost:8080/api/upload',files=files)
+
+
+
+def writeToFile(fileData):
     with io.FileIO('./test.jpg', 'w') as file:
         file.write(fileData)
-    #imageFile = open('./test.jpg', 'wb').write(fileData)
-
-    #res = requests.post(url='http://localhost:8080',
-     #               data=fileData,
-      #              headers={'Content-Type': 'application/octet-stream'})
-
-
+    
